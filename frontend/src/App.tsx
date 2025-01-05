8621ef12ff39035d058e64b6afd9ed93518b2517
Myo Thiha <mth2682004@gmail.com>
2025-01-05 23:46:05 +0000

    initialise project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-600 text-5xl">Hello</h1>
+      <Button>Click Me</Button>
+    </>
+  );
+}
+
+export default App;
