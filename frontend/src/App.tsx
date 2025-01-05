1137ee99c07dae588b6002a0301af08a252db2da
Myo Thiha <mth2682004@gmail.com>
2025-01-05 23:58:38 +0000

    clerk authentication set up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,21 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
 import { Button } from "./components/ui/button";
 
 function App() {
   return (
-    <>
-      <h1 className="text-red-600 text-5xl">Hello</h1>
-      <Button>Click Me</Button>
-    </>
+    <header>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
   );
 }
 
